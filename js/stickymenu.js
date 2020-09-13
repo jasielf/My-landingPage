2d3dc4c79bc35a428086d2cd1da48b5a1f487bff
jasielf <52427904+jasielf@users.noreply.github.com>
2020-09-13 01:16:30 -0500

    Update stickymenu.js

--- js/stickymenu.js
+++ js/stickymenu.js
@@ -1,9 +1,26 @@
 var menu= document.getElementById('menu');
 var altura = menu.offsetTop;
+
+var bmenu = $('.icon-movil-menu');
+var togMenu = $('ul');
+
 window.addEventListener('scroll', function(){
   if (window.pageYOffset > 15) {
 	menu.classList.add('stick');
   } else {
     menu.classList.remove('stick')
   }
+})
+
+bmenu.on('click', function(){
+  if(togMenu.hasClass('movil-menu')) {
+    togMenu.removeClass('movil-menu');
+  } else {
+    togMenu.addClass('movil-menu');
+  }
+})
+
+togMenu.on('click', function(){
+  togMenu.parent().siblings().find('enla');
+  togMenu.removeClass('movil-menu');
 })
